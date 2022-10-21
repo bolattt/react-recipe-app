e869bdf488e6a7cb8d5dbe605d29235bbcc5ed5c
Bo Latt <bolattny@gmail.com>
2022-10-21 05:19:14 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./components/home/Home";
+import Create from "./components/create/Create";
+import Search from "./components/search/Search";
+import Recipe from "./components/recipe/Recipe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/create">Create</Link>
+          <Link to="/recipe">Recipe</Link>
+          <Link to="/search">Search</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/recipes/:id" element={<Recipe />}></Route>
+          <Route path="*" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
