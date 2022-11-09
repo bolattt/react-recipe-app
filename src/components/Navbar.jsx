0c2f491baddfb5824a3aa4ed076be118c039726f
Bo Latt <bolattny@gmail.com>
2022-11-08 23:54:21 -0500

    use color from context in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ backgroundColor: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Panda</h1>
