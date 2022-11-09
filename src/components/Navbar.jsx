29dc151d108fe9432a2609ef4460536e3ec96845
Bo Latt <bolattny@gmail.com>
2022-11-09 00:08:10 -0500

    refactor into useTheme hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
-import { ThemeContext } from "../context/ThemeContext";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
-  const { color } = useContext(ThemeContext);
+  const { color } = useTheme();
   return (
     <div className="navbar" style={{ backgroundColor: color }}>
       <nav>
