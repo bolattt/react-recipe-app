9a0baf8d03f0d564b06ce67b6ec94b9547d689f1
Bo Latt <bolattny@gmail.com>
2022-11-11 20:53:56 -0500

    add changeMode reducer

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { act } from "react-dom/test-utils";
 
 export const ThemeContext = createContext();
 
@@ -6,6 +7,8 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_COLOR":
       return { ...state, color: action.payload };
+    case "CHNAGE_MODE":
+      return { ...state, mode: action.payload };
 
     default:
       return state;
@@ -15,13 +18,18 @@ const themeReducer = (state, action) => {
 export function ThemeProvider({ children }) {
   const [state, dispatch] = useReducer(themeReducer, {
     color: "#58249c",
+    mode: "dark",
   });
 
   const changeColor = (color) => {
     dispatch({ type: "CHANGE_COLOR", payload: color });
   };
+  const changeMode = (mode) => {
+    dispatch({ type: "CHANGE_MODE", payload: mode });
+  };
+
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
