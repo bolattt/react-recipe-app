9426295d8d5971c0fc8be5432bbf02b17ff8cbcb
Bo Latt <bolattny@gmail.com>
2022-11-13 15:08:12 -0500

    fetch one receipe from firebase

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,20 +1,39 @@
-import { useParams, useEffect } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { projectFirestore } from "../../firebase/config";
 import { useTheme } from "../../hooks/useTheme";
 import "./Recipe.css";
 function Recipe() {
   const { mode } = useTheme();
   const { id } = useParams();
-  const {
-    data: recipe,
-    isPending,
-    error,
-  } = useFetch("http://localhost:3000/recipes/" + id);
+
+  const [recipe, setRecipe] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        console.log(doc);
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError("Couldn't find recipe!");
+        }
+      });
+  }, [id]);
 
   return (
     <div className={`recipe ${mode}`}>
-      {error && "Errror"}
-      {isPending && "Loading..."}
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">"Loading..."</p>}
       {recipe && (
         <>
           <h3 key={recipe.id}>{recipe.title}</h3>
